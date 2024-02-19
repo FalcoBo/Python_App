63e88e4dd52bd0387fa3b52edec1cb942a3457f3
Theo <118967935+FalcoBo@users.noreply.github.com>
2024-02-19 13:25:57 +0100

    class database

--- functions.py
+++ functions.py
@@ -1,10 +1,58 @@
 import os
 import sqlite3
 import shutil
+import Logger
 
-def save_db(source, destination):
-    try:
-        shutil.copy(source, destination)
-        print("Database was saved successfully.")
-    except:
-        print("Database was not saved.")
+# Class to handle the database
+class Database:
+    # Constructor
+    def __init__(self, db):
+        self.db = db
+        self.connection = sqlite3.connect(self.db)
+        self.cursor = self.connection.cursor()
+        self.logger = Logger.Logger()
+
+    # Function to create a table in the database
+    def create_table(self, table_name, columns):
+        create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)})"
+        self.cursor.execute(create_table_query)
+        self.conn.commit()
+
+    # Function to iclear a table in the database
+    def clear_table(self, table_name):
+        clear_table_query = f"DELETE FROM {table_name}"
+        self.cursor.execute(clear_table_query)
+        self.conn.commit()
+
+    # Function to save the database
+    def save_db(source, destination):
+        try:
+            shutil.copy(source, destination)
+            print("Database was saved successfully.")
+        except:
+            print("Database was not saved.")
+
+    # Function to remove the database
+    def remove_db(db):
+        try:
+            os.remove(db)
+            print("Database was removed successfully.")
+        except:
+            print("Database was not removed.")
+        
+    # Function to clear the database
+    def clear_db(db):
+        try:
+            connection = sqlite3.connect(db)
+            cursor = connection.cursor()
+            cursor.execute('DELETE FROM users')
+            connection.commit()
+            connection.close()
+            print("Database was cleared successfully.")
+        except:
+            print("Database was not cleared.")
+
+    # Function to close the database
+    def close_db(self):
+        self.connection.close()
+        self.logger.log("The connection to the database was closed successfully.")
